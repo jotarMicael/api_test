f414b644d64c344f53660ec2eaa04d7ac8074a92
jotarMicael <gameuxlegacy@gmail.com>
2021-05-27 20:49:41 -0300

    finished

--- views/api_test.php
+++ views/api_test.php
@@ -1,14 +1,15 @@
 <?php
 
+if (!$_GET) {
+    $result = json_decode(file_get_contents('https://www.freetogame.com/api/games'), true);
+} else {
+    if ($_GET["category"] == 'on') {
+        var_dump("hola");
+        die;
+    }
 
-function callWebService()
-{
-    return json_decode(file_get_contents('https://www.freetogame.com/api/games'), true);;
+    $result = json_decode(file_get_contents('https://www.freetogame.com/api/games?category=' . $_GET["category"]), true);
 }
-
-$result = callWebService();
-
-
 ?>
 
 
@@ -24,10 +25,11 @@ $result = callWebService();
 
     <title>Free games</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
-
     <!-- Bootstrap core CSS -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!------------------------>
 
     <!-- Custom styles for this template -->
     <link href="../css/album.css" rel="stylesheet">
@@ -35,6 +37,7 @@ $result = callWebService();
     <link href="../css/preloader.css" rel="stylesheet">
     <link href="../css/hover_icons.css" rel="stylesheet" media="all">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" media="all" onload="this.media='all'">
+    <!------------------------------------->
 
 </head>
 
@@ -78,15 +81,34 @@ $result = callWebService();
 
         <section class="jumbotron text-center">
             <div class="container">
-            
+
                 <h1> <i class="fas fa-search mr-2"></i>Find Games</h1>
                 <p class="lead text-muted">Something short and leading about the free games.</p>
 
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="all" value="option1">
+                    <label class="form-check-label" for="exampleRadios1">
+                        All
+                    </label>
+
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="shooter"  value="option2" <?php if($_GET['category'] == "shooter"){ echo "checked";} ?> >
+                    <label class="form-check-label" for="exampleRadios2">
+                        Shooter
+                    </label>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="mmorpg" value="option3"<?php if($_GET['category'] == "mmorpg"){ echo "checked";} ?> >
+                    <label class="form-check-label" for="exampleRadios3">
+                        Mmorpg
+                    </label>
+                </div>
+
                 <input type="text" id="formulario" class="searchInput form-control form-control-lg deep-dark" placeholder="Search for games" autocomplete="off">
                 <button class="btn btn-info mt-2" id="boton">Search game</button>
-
-                </p>
             </div>
+
+
+
         </section>
 
         <div class="album py-5 bg-light">
@@ -94,11 +116,7 @@ $result = callWebService();
                 <div class="container">
 
                     <div id="resultado" class="row">
-
-
-
-
-
+                        <!-- body -->
                     </div>
 
 
@@ -113,11 +131,12 @@ $result = callWebService();
                 <!-- Modal content-->
                 <div class="modal-content">
                     <div id="mdHeader" class="modal-header">
+                        <!-- header -->
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
 
                     </div>
                     <div id="mdBody" class="modal-body">
-
+                        <!-- body -->
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -147,6 +166,7 @@ $result = callWebService();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.7/holder.js" integrity="sha512-uhp2Ee4MNexF4HNrWF5Vo82DIq6bvfdEcDJEqOAVy7q2h2I4HsZTFgfEoHt7+j/Ez2cEeJ0yyrZZxcGeY9aT+A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!---------------------------------------------------------------->
     <script>
         const games = <?php echo json_encode($result); ?>
 
@@ -178,7 +198,7 @@ $result = callWebService();
                                             <p class="card-text">${game.short_description}</p>
                                              <div class="d-flex justify-content-between align-items-center">
                                                 <div class="btn-group">
-                                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                                    <button type="button" class="btn btn-secondary btn-rounded" data-toggle="modal" data-target="#myModal">
                                                         View more...
                                                     </button>                                       
                                                 </div>
@@ -197,7 +217,7 @@ $result = callWebService();
                     <div class="container"> <p> Platform: <a href="#">${game.platform}</a> </p> </div>
                     <div class="container"> <p> Release Date: <a href="#">${game.release_date}</a> </p> </div>
                     <div class="container"> <p> Developer: <a href="#">${game.developer}</a> </p> </div>
-                        <div class="container"> <p> <a href="${game.game_url}"> Web page </a> </p> </div>
+                        <div class="container"> <p> <a href="${game.game_url}"> Official page </a> </p> </div>
                     </div>   
                     `
 
@@ -221,11 +241,11 @@ $result = callWebService();
                     <figure><img class="card-img-top" src="${game.thumbnail}" alt="Card image cap"></figure>
                 </div>
                         <div id="resultado" class="card-body">
-                            <h4 class="card-text">${game.title}</h4>
+                         <h4 class="card-text"> <a href="#">${game.title}</a></h4>
                                 <p class="card-text">${game.short_description}</p>
                                  <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                        <button type="button" class="btn btn-secondary btn-rounded" data-toggle="modal" data-target="#myModal">
                                             View more...
                                         </button>                                       
                                     </div>
@@ -235,14 +255,14 @@ $result = callWebService();
                         </div>
                     </div>       
         `
-                mdHeader.innerHTML = `<h4 class="card-text">Game: ${game.title}</h4>`;
+                mdHeader.innerHTML = `  <h4 class="card-text">Game: ${game.title}</h4>`;
                 mdBody.innerHTML = `<img class="card-img-top" src="${game.thumbnail}" alt="Card image cap"><hr>
         <div class="container">
         <div class="container"> <p> Genre: <a href="#">${game.genre}</a> </p> </div>
         <div class="container"> <p> Platform: <a href="#">${game.platform}</a> </p> </div>
         <div class="container"> <p> Release Date: <a href="#">${game.release_date}</a> </p> </div>
         <div class="container"> <p> Developer: <a href="#">${game.developer}</a> </p> </div>
-            <div class="container"> <p> <a href="${game.game_url}"> Web page </a> </p> </div>
+            <div class="container"> <p> <a href="${game.game_url}"> Official page </a> </p> </div>
         </div>   
         `
             }
@@ -264,7 +284,28 @@ $result = callWebService();
     </script>
 
     <!-- -->
-    
+    <script>
+        $(document).ready(function() {
+
+            $('#shooter').click(function() {
+
+                window.location.href = `?category=shooter`;
+
+            })
+            $('#mmorpg').click(function() {
+
+                window.location.href = `?category=mmorpg`;
+            })
+
+            $('#all').click(function() {
+
+                url = "http://localhost/free_games/views/api_test.php";
+                $(location).attr('href', url);
+            })
+
+
+        });
+    </script>
 </body>
 
 </html>
